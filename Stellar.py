0fc70fbe2093abd6c40b86ef594b6f1b865c9e57
LAPTOP-VKBPV7PQ\Grace <gfiacco12@gmail.com>
2018-10-23 16:11:59 -0400

    Here will be the main code

---
+++ Stellar.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 16:09:31 2018
+
+@author: Grace
+"""
+#imports
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+#Start of code
