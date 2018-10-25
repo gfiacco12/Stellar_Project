067e9c9daeb899c7ca2d44bfea426fc47facbed6
drm4487 <dalemercado97@gmail.com>
2018-10-25 18:15:27 -0400

    first commit

--- Stellar.py
+++ Stellar.py
@@ -15,16 +15,22 @@ def star(Msolar, Lsolar, Teff, X, Z):
     
     #define variables
     #ALL UNITS IN CGS
-    Nstart=10 #number of steps for starting equations
-    Nstop=999 #max number of steps
-    Igoof=-1 #final model condition flag
-    ierr=0 
-    P0=0 #surface pressure
-    T0=0 #surface temp
-    dlPlim=99.9 
-    Rsun=6.9599E10 #radius of sun
-    Msun=1.989E33 #mass of sun
-    Lsun=3.826E33 #sun luminosity
-    sigma= 5.67E-5 #stefan boltzmann
-    c=3E8 #speed of light
-    a=(4*sigma)/c #rad pressure constant
+    Nstarti = 10 #number of steps for starting equations
+    Nstop = 999 #max number of steps
+    Igoof = -1 #final model condition flag
+    ierr = 0 
+    P0 = 0 #surface pressure
+    T0 = 0 #surface temp
+    dlPlim = 99.9 
+    Rsun = 6.9599E10 #radius of sun
+    Msun = 1.989E33 #mass of sun
+    Lsun = 3.826E33 #sun luminosity
+    sigma = 5.67E-5 #stefan boltzmann
+    c = 3E8 #speed of light
+    a = (4*sigma)/c #rad pressure constant
+    G = 6.67259E-8 #grav constant
+    k_B = 1.380658E-16 #boltzman constants
+    m_H = 1.673534E-24 #mass hydrogen
+    pi = np.pi
+
+    
